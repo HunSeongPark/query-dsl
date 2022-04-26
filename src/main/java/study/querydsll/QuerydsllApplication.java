72d8b18fbf0084b4449cfc6fabff9fc10281ed84
Hunseong <phunseong@gmail.com>
2022-04-26 17:53:37 +0900

    initial commit

---
+++ src/main/java/study/querydsll/QuerydsllApplication.java
@@ -0,0 +1,13 @@
+package study.querydsll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydsllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydsllApplication.class, args);
+	}
+
+}
