30b077c638fe24dad6fe3121e8af78507eda85c0
Hunseong <phunseong@gmail.com>
2022-04-26 18:07:45 +0900

    QType, Lombok test

--- src/test/java/study/querydsll/QuerydsllApplicationTests.java
+++ src/test/java/study/querydsll/QuerydsllApplicationTests.java
@@ -1,13 +1,39 @@
 package study.querydsll;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsll.entity.Hello;
+import study.querydsll.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydsllApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello findHello = queryFactory
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(findHello).isEqualTo(hello);
 	}
 
 }
