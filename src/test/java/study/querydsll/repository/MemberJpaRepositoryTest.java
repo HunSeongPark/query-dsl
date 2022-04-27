e3dddeeba464048d0846cb3df7e36f7cf075e280
Hunseong <phunseong@gmail.com>
2022-04-27 18:17:38 +0900

    MemberJpaRepository - dynamic query(BooleanBuilder) test

--- src/test/java/study/querydsll/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsll/repository/MemberJpaRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsll.dto.MemberSearchCond;
+import study.querydsll.dto.MemberTeamDto;
 import study.querydsll.entity.Member;
+import study.querydsll.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -62,5 +65,30 @@ class MemberJpaRepositoryTest {
         // then
         assertThat(allResult).containsExactly(member);
         assertThat(usernameResult).containsExactly(member);
+    }
+    
+    @Test
+    void searchTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCond condition = new MemberSearchCond(
+                null, "teamB", 35, 40);
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("username").containsExactly("member4");
+        assertThat(result).extracting("teamName").containsExactly("teamB");
     }
 }
