01e878e10239b309e3351ec57cdc8f85939bdda8
Hunseong <phunseong@gmail.com>
2022-04-27 18:51:54 +0900

    MemberRepository - search test

--- src/test/java/study/querydsll/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsll/repository/MemberRepositoryTest.java
@@ -47,4 +47,30 @@ class MemberRepositoryTest {
         assertThat(allResult).containsExactly(member);
         assertThat(usernameResult).containsExactly(member);
     }
+
+    @Test
+    void searchTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCond condition = new MemberSearchCond(
+                null, "teamB", 35, 40);
+
+        List<MemberTeamDto> whereResult = memberRepository.search(condition);
+
+        assertThat(whereResult).extracting("teamName").containsExactly("teamB");
+    }
 }
