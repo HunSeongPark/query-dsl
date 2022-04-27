71426ecbb479358beadb5e333f72f85bcd931476
Hunseong <phunseong@gmail.com>
2022-04-27 19:06:28 +0900

    MemberController - API에 Paging 적용

--- src/main/java/study/querydsll/controller/MemberController.java
+++ src/main/java/study/querydsll/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsll.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsll.dto.MemberSearchCond;
 import study.querydsll.dto.MemberTeamDto;
 import study.querydsll.repository.MemberJpaRepository;
+import study.querydsll.repository.MemberRepository;
 
 import java.util.List;
 
@@ -17,9 +20,20 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCond condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCond condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCond condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
