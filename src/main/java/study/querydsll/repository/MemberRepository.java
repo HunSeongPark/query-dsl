f6223a094efff61887d0813b088b2ff494761f26
Hunseong <phunseong@gmail.com>
2022-04-27 18:46:16 +0900

    MemberRepository - Spring Data Jpa Repository

---
+++ src/main/java/study/querydsll/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package study.querydsll.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsll.entity.Member;
+
+import java.util.List;
+
+/**
+ * Created by Hunseong on 2022/04/27
+ */
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+}
